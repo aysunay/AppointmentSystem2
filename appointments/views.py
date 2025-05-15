6a981a809305f50aed580d1079d748379533e1a1
kadir onur <hancikadironur@gmail.com>
2025-05-15 22:07:24 +0300

    ilk sayfa deneme

---
+++ appointments/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def appointment_list(request):
+    return HttpResponse("<h1>Randevu Listesi</h1><p>Burada randevular listelenecek.</p>")
+
+def create_appointment(request):
+    return HttpResponse("<h1>Yeni Randevu Oluştur</h1><p>Burada randevu ekleme formu olacak.</p>") 
+def appointment_list(request):
+    return HttpResponse("<h1>Randevu Listesi</h1><p>Burada randevular listelenecek.</p>")
+
+def create_appointment(request):
+    return HttpResponse("<h1>Yeni Randevu Oluştur</h1><p>Burada randevu ekleme formu olacak.</p>")
